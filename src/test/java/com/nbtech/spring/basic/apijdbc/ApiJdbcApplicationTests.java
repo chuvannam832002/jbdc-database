0c3fb1d2d1db83f0e51adf25f949efb38754d533
832002 <chuvannam832002>
2022-12-27 07:01:52 +0700

    Practice and Create with Spring-JDBC-API

---
+++ src/test/java/com/nbtech/spring/basic/apijdbc/ApiJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbtech.spring.basic.apijdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
