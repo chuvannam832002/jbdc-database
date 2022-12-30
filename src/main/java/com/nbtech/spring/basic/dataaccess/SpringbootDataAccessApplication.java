fd4c2c4323e700c4f6d1c4931e3a917f0181fc57
832002 <chuvannam832002>
2022-12-30 07:46:34 +0700

    Demo with SpringDataJPA with sql query

--- src/main/java/com/nbtech/spring/basic/dataaccess/SpringbootDataAccessApplication.java
+++ src/main/java/com/nbtech/spring/basic/dataaccess/SpringbootDataAccessApplication.java
@@ -1,5 +1,7 @@
 package com.nbtech.spring.basic.dataaccess;
 
+import com.nbtech.spring.basic.dataaccess.SpringDataJPA.PlayerEntity;
+import com.nbtech.spring.basic.dataaccess.SpringDataJPA.PlayerRepository;
 import com.nbtech.spring.basic.dataaccess.SpringJdbcApi.Player;
 import com.nbtech.spring.basic.dataaccess.SpringJdbcApi.PlayerDAO;
 import com.nbtech.spring.basic.dataaccess.SpringJdbcApi.TournamentDAO;
@@ -18,6 +20,8 @@ public class SpringbootDataAccessApplication implements CommandLineRunner {
 	@Autowired
 	TournamentDAO tournamentDAO;
 	@Autowired
+	PlayerRepository playerRepository;
+	@Autowired
 	PlayerDAO playerDao;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDataAccessApplication.class, args);
@@ -26,21 +30,53 @@ public class SpringbootDataAccessApplication implements CommandLineRunner {
 	}
 	@Override
 	public void run(String... args) throws Exception {
-tournamentDAO.createTournamentTable();
+SpringDataJPA();
+
+	}
+	private void SpringDataJPA() {
+		// ex1
+		logger.info("\n\n>> Inserting Player: {}\n", playerRepository.insertPlayer(
+				new PlayerEntity("Djokovic", "Serbia", Date.valueOf("1987-05-22"), 81)));
+
+		logger.info("\n\n>> Inserting Player: {}\n", playerRepository.insertPlayer(
+				new PlayerEntity("Monfils", "France", Date.valueOf("1986-09-01"), 10)));
+
+		// ex2
+		logger.info("\n\n>> Player with id 2: {}\n", playerRepository.getPlayerById(2));
+
+		// ex3
+		logger.info("\n\n>> Inserting Player: {}\n", playerRepository.insertPlayer(
+				new PlayerEntity("Thiem", "Austria",
+						new Date(System.currentTimeMillis()), 17)));
+		logger.info("\n\n>> Updating Player with Id 3: {}\n", playerRepository.updatePlayer(
+				new PlayerEntity(3, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17)));
+		logger.info("\n\n>> Player with id 3: {}\n", playerRepository.getPlayerById(3));
+
+		//delete player
+		playerRepository.deleteById(2);
+	}
+
+	private void SpringJdbcApi() {
+		tournamentDAO.createTournamentTable();
+
 		logger.info("French Players: {}", playerDao.getPlayerByNationality("France"));
+
+		/*
 		logger.info("Inserting Player 4: {}", playerDao.insertPlayer(
-				new Player(4, "Thiem", "Austria", new Date(System.currentTimeMillis()), 17 ))
+				new Player (4, "Thiem", "Austria", new Date(System.currentTimeMillis()), 17 ))
 		);
 
 		logger.info("Updating Player with Id 4: {}", playerDao.updatePlayer(
 				new Player(4, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17))
 		);
 
-	//	logger.info("Deleting Player with Id 2: {}", playerDao.deletePlayerById(2));
+		logger.info("Deleting Player with Id 2: {}", playerDao.deletePlayerById(2));
 
 		logger.info("All Players Data: {}", playerDao.getAllPlayers());
 
 		logger.info("Player with Id 3: {}", playerDao.getPlayerById(3));
-
+		 */
 	}
+
+
 }
